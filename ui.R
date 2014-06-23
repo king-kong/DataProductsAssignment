3d8689feae13457c048f239577afbbb821eac7b3
king-kong <robert.kok@gmail.com>
2014-06-23 07:47:45 -0400

    Create ui.R

---
+++ ui.R
@@ -0,0 +1,32 @@
+
+# This is the user-interface definition of a Shiny web application.
+# You can find out more about building applications with Shiny here:
+# 
+# http://www.rstudio.com/shiny/
+#
+
+library(shiny)
+
+shinyUI(pageWithSidebar(
+  
+  # Application title
+  headerPanel("City of Toronto Ward Data"),
+  
+  # Sidebar with a slider input for number of bins
+  sidebarPanel(
+    p("Please slide the slider to choose the Toronto Ward you wish to select!"),
+    sliderInput("ward",
+                "Toronto Ward:",
+                min = 1,
+                max = 44,
+                value = 1)
+  ),
+  
+  # Show a plot of the generated distribution
+  mainPanel(
+    htmlOutput("ward"),
+    plotOutput("res"),
+    plotOutput("com"),
+    plotOutput("election2010")
+  )
+))
